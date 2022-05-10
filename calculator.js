27108a64f535030766a20389bc85e74d419f03b0
yosefHesham <yousefmobdev@gmail.com>
2022-05-10 16:37:14 +0200

    Create calculator class

---
+++ calculator.js
@@ -0,0 +1,18 @@
+class Calculator {
+  static addNumber(a,b) {
+    return a + b;
+  }
+  static subtractNumber(a,b) {
+    return a - b;
+  }
+
+  static divideNumber(a,b) {
+    return a /b;
+  }
+
+  static multiplyNumber(a,b) {
+    return a * b;
+  }
+}
+
+module.exports = Calculator
