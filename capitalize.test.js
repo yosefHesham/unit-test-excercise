e42b047bcc9bff6a85d228adb9d5cbc5abce96e2
yosefHesham <yousefmobdev@gmail.com>
2022-05-10 17:07:22 +0200

    Add test for capitalize

---
+++ capitalize.test.js
@@ -0,0 +1,12 @@
+it("should return string with first char capitalized",()=> {
+  //arrange
+  const word = "yousef"
+
+  // act
+
+  const result = capitalize(word)
+
+  //assert
+  expect(result).toBe("Yousef")
+
+})
